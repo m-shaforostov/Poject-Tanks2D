e1c5ce12b41ac18f00b4f9c4f6696c44bc5ced1d
m-shaforostov <maxim.shaforostov@gmail.com>
2025-06-21 12:54:50 +0200

    preparation for the game

---
+++ src/main/java/com/example/projecttanks/Player.java
@@ -0,0 +1,5 @@
+package com.example.projecttanks;
+
+public enum Player {
+    ONE, TWO
+}
---
+++ src/main/java/com/example/projecttanks/Tank.java
@@ -0,0 +1,6 @@
+package com.example.projecttanks;
+
+public class Tank {
+    public Vector2D position = new Vector2D();
+    public Vector2D direction = new Vector2D();
+}
